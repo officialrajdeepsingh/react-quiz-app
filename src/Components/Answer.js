3f29bc7fae00a15c34e603d5e2a5e20d486ef6fb
Rajdeep singh <32416397+officialrajdeepsingh@users.noreply.github.com>
2019-11-28 17:57:19 +0530

    str folder

---
+++ src/Components/Answer.js
@@ -0,0 +1,36 @@
+import React, { Component, Fragment } from "react";
+
+class Answer extends Component {
+ 
+//  create state
+  state = {
+    Answers: this.props.anwer,
+    Clickcheck:true,
+    rightAnaswer: this.props.rightAnaswer
+  };
+
+  // Event on button
+  onAnswer = () => {
+   
+    this.setState({ 
+      Clickcheck:false
+    });
+  
+  };
+  
+  render() {
+
+    
+
+    return (
+      <Fragment>
+        { this.state.Clickcheck ? this.state.Answers.map(ans => {
+          return <button onClick={this.onAnswer}> {ans}</button>;
+        }) : <button > {this.state.rightAnaswer}</button>  } 
+
+      </Fragment>
+    );
+  }
+}
+
+export default Answer;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import Header from "./Components/Header";
+import Quiz from "./Components/Quiz";
+
+function App() {
+  return (
+
+    <Fragment>
+
+      {/* header Components */}
+        <Header />
+
+      {/* Quiz Components */}
+        <Quiz />
+      
+    </Fragment> 
+
+  )
+}
+
+const rootElement = document.getElementById("root");
+// reader method for react 
+ReactDOM.render(<App />, rootElement);
